1e101c4b3469bbe2ef2a43ba6aec9c5af57a1c1d
Milton Cody <cody.mltn.dev@outlook.com>
2017-06-12 23:21:19 -0500

    use lazy kernal

--- HelloWorld-CroweHorwath/Core/KernelEngine.cs
+++ HelloWorld-CroweHorwath/Core/KernelEngine.cs
@@ -1,21 +1,18 @@
 ﻿using Ninject;
+using System;
 
 namespace HelloWorld_CroweHorwath
 {
     public sealed class KernalEngine {
-        private static KernalEngine instance;
+        private static readonly Lazy<KernalEngine> instance = new Lazy<KernalEngine>(() => new KernalEngine());
         public IKernel GetKernal { get; }
 
         private KernalEngine() {
             GetKernal = new StandardKernel();
         }
 
-        static KernalEngine() {
-            instance = new KernalEngine();
-        }
-
         public static KernalEngine Instance {
-            get { return instance; }
+            get { return instance.Value; }
         }
     }
 }
